6b9150200ff7c5cdfedad4f05e82077aadf0dc79
Shutao Chen <56904214+ShutaoChen97@users.noreply.github.com>
2023-11-24 22:18:00 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# IIDLPepPI=/home/cst/GitHub/IIDL-PepPI
+# Please modify < INSTALL_DIR > to the absolute installation path of IIDL-PepPI.
 IIDLPepPI = <INSTALL_DIR>
 
 python test.py  -method_path $IIDLPepPI -pep_fasta $IIDLPepPI/example/example_peptide_1.fasta \
